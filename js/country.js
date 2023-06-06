649ba611ed140986fc8b29bf26841fde70bf269e
chumki <chumkimodak900@gmail.com>
2023-06-06 12:28:07 +0530

    destructuring

--- js/country.js
+++ js/country.js
@@ -4,17 +4,36 @@ const loadCountries = () => {
         .then(data => displayCountries(data))
 }
 const displayCountries = countries => {
-    // console.log(countries)
+    console.log(countries)
     const allCountriesHTML = countries.map(country => getcountryHTML(country))
     // console.log(allCountriesHTML[0])
     const countryContainer = document.getElementById('country-content');
     countryContainer.innerHTML = allCountriesHTML.join(``)
 }
-const getcountryHTML = country => {
+const getcountryHTML = ({ name, flags, area, region }) => {
     return `
-    <div class=country>
-        <h2>${country.name.common}</h2>
-        <img src="${country.flags.png}">
-    </div>`
+         <div class=country>
+             <h2>${name.common}</h2>
+             <img src="${flags.png}">
+             <p>${area}</p>
+             <p>${region}</p>
+         </div>`
 }
+// const getcountryHTML = country => {
+//     //destructuring option1
+//     const { name, flags } = country;
+//     return `
+//     <div class=country>
+//         <h2>${country.name.common}</h2>
+//         <img src="${country.flags.png}">
+//     </div>`
+// }
+// const getcountryHTML = country => {
+//     //original
+//     return `
+//     <div class=country>
+//         <h2>${country.name.common}</h2>
+//         <img src="${country.flags.png}">
+//     </div>`
+// }
 loadCountries()
